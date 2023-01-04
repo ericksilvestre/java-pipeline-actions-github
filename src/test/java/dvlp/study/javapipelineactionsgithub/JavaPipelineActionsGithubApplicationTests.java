ff74b8683f81e6ab52da7a8aeceb02df703a53a6
Erick <erickaguiarsilvestre@gmail.com>
2023-01-04 20:02:47 -0300

    commit init

---
+++ src/test/java/dvlp/study/javapipelineactionsgithub/JavaPipelineActionsGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package dvlp.study.javapipelineactionsgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPipelineActionsGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
